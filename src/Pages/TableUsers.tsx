46cfa4f095aa3cb0cf3ef16be1b4b1364087bb25
MNATorres <mtri21019@gmail.com>
2022-08-29 12:07:41 -0300

    se agregan cambios a la tabla

--- src/Pages/TableUsers.tsx
+++ src/Pages/TableUsers.tsx
@@ -31,18 +31,18 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   function createData(
     name: string,
     calories: number,
-    fat: number,
-    carbs: number,
+    fat: string,
+    carbs: string,
   ) {
     return { name, calories, fat, carbs };
   }
   
   const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24),
-    createData('Ice cream sandwich', 237, 9.0, 37),
-    createData('Eclair', 262, 16.0, 24),
-    createData('Cupcake', 305, 3.7, 67),
-    createData('Gingerbread', 356, 16.0, 49),
+    createData('Frozen yoghurt', 159, '6.0', '24'),
+    createData('Ice cream sandwich', 237, '9.0', '37'),
+    createData('Eclair', 262, '16.0', '24'),
+    createData('Cupcake', 305, '3.7', '67'),
+    createData('Gingerbread', 356, '16.0', '49'),
   ];
   
 
